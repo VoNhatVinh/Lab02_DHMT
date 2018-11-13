b12305d3415f35e1fa4c59e50f05e799316d71dc
VoNhatVinh <yenlang2017@gmail.com>
2018-11-13 23:02:49 +0700

    fix poligol time

--- Lab01_DHMT/Form1.cs
+++ Lab01_DHMT/Form1.cs
@@ -63,7 +63,7 @@ namespace Lab01_DHMT
             gl.Viewport(0, 0, openGLControl.Width, openGLControl.Height);
             gl.Ortho2D(0, openGLControl.Width, 0, openGLControl.Height);
         }
-
+         
         /*Draw shape*/
         private void openGLControl_OpenGLDraw(object sender, RenderEventArgs args)
         {
@@ -126,7 +126,6 @@ namespace Lab01_DHMT
                 clock = DateTime.Now.Subtract(startTime);
             }
 
-
             if (mode == 2)//draw polygon
             {
                 if (e.Button == MouseButtons.Right)
@@ -139,13 +138,14 @@ namespace Lab01_DHMT
                 }else
                 if (e.Button == MouseButtons.Left)
                 {
+                    //startTime = DateTime.Now;
                     plg.addPoint(e.Location);
                     mouseLeft = true;
                     clock += DateTime.Now.Subtract(startTime);
                 }
             }
 
-            if (chooseIcon < 1)
+            if (chooseIcon < 1 && mode!=2)
                 clock = TimeSpan.Zero;
         }
       
--- Lab01_DHMT/Shapes/polygon.cs
+++ Lab01_DHMT/Shapes/polygon.cs
@@ -27,11 +27,12 @@ namespace Lab01_DHMT.Shapes
 
         public void drawPolygon(OpenGLControl glControl, Color _clr, float size)
         {
+            base.Draw(glControl);
             glControl.OpenGL.Begin(OpenGL.GL_LINE_LOOP);
             glControl.OpenGL.LineWidth(size);
             glControl.OpenGL.Color(_clr.R / 255.0, _clr.G / 255.0, _clr.B / 255.0);
 
-           plg.ForEach(plgon => glControl.OpenGL.Vertex(plgon.X, glControl.Height- plgon.Y));    
+            plg.ForEach(plgon => glControl.OpenGL.Vertex(plgon.X, glControl.Height- plgon.Y));    
             glControl.OpenGL.End();
             glControl.OpenGL.Flush();
         }
